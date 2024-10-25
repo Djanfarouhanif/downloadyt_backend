ea0081b4058e64620025abd9a966e0370c5c2a9a
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-10-25 21:38:33 +0000

    reveiw code

--- Youtube/main.py
+++ Youtube/main.py
@@ -3,26 +3,13 @@ from pytube import YouTube
 
 #Fonction pour télécharger la video via l'URL de la video
 
-def download_video(url):
-    
-    #Création d'un object YouTube
-    yt = YouTube(url)
-
-        # Sélection de la meilleure qualité vidéo disponible
-    stream = yt.streams.get_highest_resolution()
-
-    try: 
-        #Téléchargement de la vidéo 
-        
-        stream.download()
-        print('Téléchargement terminée !')
-
-    except Exception as e :
-        print(f' Errure lors de téléchargement : {e}')
+def  download_video(url):
+    try:
+        yt = YouTube(url)
+        print("terminé")
+    except :
+        print("erreur")
 
 
-url_video = input('urls') 
-
-download_video(url_video)
-
-
+url = "https://youtu.be/w8PVpWxE5_8?si=Q2eijSN3-ni-zwV-"
+download_video(url)
