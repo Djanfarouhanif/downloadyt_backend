3485c874a4f50d80d25f036922e83a3a0afe3d11
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-10-27 16:39:25 +0000

    add new fonction download_audio

--- Youtube/main.py
+++ Youtube/main.py
@@ -13,5 +13,23 @@ def download_video(url):
     except Exception as e:
         print(f"Erreur lors du téléchargemment avec yt-dlp: {e}")
 
-url = 'https://youtu.be/9bZkp7q19f0'
-download_video(url)
+
+# Fonction pour télécharger les audio
+def download_audio(url):
+    ydl_opts = {
+        'format': 'bestaudio/best', #Télécharge uniquemment l'audio
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3', # Convertir en mp3
+            'preferredquality': '192', # Qualité audio
+        }],
+        'outtmpl': '%(title)s.%(ext)s', # Nom de fichier basé sur le titre de la video
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        print('Téléchargement audio réussi !')
+
+# url = 'https://youtu.be/8hO7sdScJAM?si=Ggt1xlToY9HC8hrf'
+# download_audio(url)
+#download_video(url)
