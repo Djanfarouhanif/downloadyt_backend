79c34c33a069cb1e69a4c10c5077c41350889f3b
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-11-10 15:30:21 +0000

    review code

--- core/settings.py
+++ core/settings.py
@@ -131,7 +131,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200"
+    "http://localhost:4200",
+    "https://downyout.netlify.app/"
 ]
 
 # configuration pour le deployement
